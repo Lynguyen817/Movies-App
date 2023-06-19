f8e11f02c36712322c776443c2028922cc25eff8
Lynguyen817 <lynguyen1708@gmail.com>
2023-06-19 18:08:37 -0400

    Edit storage_csv file

--- storage_csv.py
+++ storage_csv.py
@@ -1,8 +1,8 @@
+import os.path
 from istorage import IStorage
 import json
 import csv
 import requests
-import pandas as pd
 
 
 API_KEY = "cfb1ce63"
@@ -11,21 +11,50 @@ API_KEY = "cfb1ce63"
 class StorageCsv(IStorage):
     """ Inherits from Storage and implements its functions."""
 
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, storage_csv):
+        self.storage_csv = storage_csv
+        self._create_storage_if_not_exist()
+
+    def _create_storage_if_not_exist(self):
+        """
+            Check is the storage_csv file exist, if it does,
+            the function return without doing anything.
+            If not, create a new storage CSV file
+        """
+        if os.path.exists(self.storage_csv):
+            return
+
+        with open(self.storage_csv, 'w') as csvfile:
+            fieldnames = ['title', 'rating', 'year', 'image']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+    def _load_db(self):
+        """ Load the data from the database."""
+        data = {}
+        with open(self.storage_csv, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data[row['title']] = row
+        return data
+
+    def _save_db(self, movies):
+        """ Save the data to the csv file."""
+        with open(self.storage_csv, 'w') as csvfile:
+            fieldnames = ['title', 'rating', 'year', 'image']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for movie_data in movies.values():
+                writer.writerow(movie_data)
 
     def list_movies(self):
         """
             Returns a dictionary of dictionaries that
             contains the movies information in the database.
         """
-        with open("movies.csv", "r") as csvfile:
-            movies_data = csv.DictReader(csvfile)
-            current_dict = {}
-            for row in movies_data:
-                new_dict = {row["title"]: {k: v for k, v in row.items() if k != "title"}}
-                current_dict.update(new_dict)
-            return current_dict
+        list_movies = self._load_db()
+        return list_movies
 
     def add_movie(self, title):
         """
@@ -42,7 +71,7 @@ class StorageCsv(IStorage):
         with open("movies.csv", "r") as handle:
             csv.reader(handle)
 
-        csv_line = [movies_data["Title"], movies_data["Year"], movies_data["imdbRating"], movies_data["Poster"]]
+        csv_line = [movies_data["Title"], movies_data["imdbRating"], movies_data["Year"], movies_data["Poster"]]
 
         with open("movies.csv", "a", newline="") as saved_file:
             writer = csv.writer(saved_file)
@@ -51,38 +80,15 @@ class StorageCsv(IStorage):
 
     def delete_movie(self, title):
         """Deletes a movie from the movies database"""
-        exist_movies_data = self.list_movies()
-        #print(exist_movies_data)
-
-        # df = pd.read_csv('movies.csv')
-        # df = df.drop(df[df.title==exist_movies_data[title]].index)
-        # df.to_csv('movies.csv', index= False)
-        # print(exist_movies_data)
-
-        # with open("movies.csv", "w", newline="") as output:
-        #     writer = csv.writer(output)
-        #     for key, value in exist_movies_data.items():
-        #         if key != title:
-        #             writer.writerow(key)
-        #             print(f'{key}:{value["rating"]}')
-
+        exist_movies_data = self._load_db()
         del exist_movies_data[title]
-        print(exist_movies_data)
-        for key, value in exist_movies_data.items():
-            csv_line = [key, value["year"], value["rating"], value["image"]]
-
-            with open("movies.csv", "a", newline="") as saved_file:
-                writer = csv.writer(saved_file)
-                csvwriter = writer.writerow(csv_line)
-                return csvwriter
+        self._save_db(exist_movies_data)
 
     def update_movie(self, title, new_rating):
         """Updates a movie from the movies database with a new rating"""
-        movies_data = self.list_movies()
+        movies_data = self._load_db()
         for key, val in movies_data.items():
             if title == key:
                 val["rating"] = new_rating
-            with open("movies.csv", "w", newline="") as save_file:
-                writer = csv.writer(save_file)
-                csvwriter = writer.writerow(movies_data)
-                return csvwriter
+                self._save_db(movies_data)
+
