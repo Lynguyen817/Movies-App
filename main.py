58eb1a5f6787196d6392f26d7f599078773d067c
Lynguyen817 <lynguyen1708@gmail.com>
2023-06-19 18:27:06 -0400

    Check the json file

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from storage_csv import StorageCsv
 
 
 def main():
-    storage = StorageCsv('movies.csv')
-    #storage = StorageJson('movies.json')
+    #storage = StorageCsv('movies.csv')
+    storage = StorageJson('movies.json')
     movie_app = MovieApp(storage)
     movie_app.run()
 
